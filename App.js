a18699f4b8d03a28d78ac3c4155634aac5df9a99
Amonoo <benjaminamonoowilberforce@outlook.com>
2024-06-01 22:11:49 +0000

    edit the App component to import the custom components into the Appp component and style it accordingly

--- App.js
+++ App.js
@@ -1,20 +1,127 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, FlatList, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import Header from './components/Header';
+import CategoryCard from './components/CategoryCard';
+import TaskItem from './components/TaskItem';
 
-export default function App() {
+const categories = [
+  { category: 'Programing', tasksCount: 6, imageUri: 'programing' },
+  { category: 'Study', tasksCount: 1, imageUri: 'study' },
+  { category: 'Fitness', tasksCount: 12, imageUri: 'fitness' },
+  { category: 'Games', tasksCount: 12, imageUri: 'games' },
+  { category: 'Creative Arts', tasksCount: 10, imageUri: 'creative_arts' },
+  { category: 'Photography', tasksCount: 8, imageUri: 'photography' },
+  { category: 'Travel', tasksCount: 5, imageUri: 'travel' },
+  { category: 'Music', tasksCount: 6, imageUri: 'music' },
+  { category: 'Party', tasksCount: 9, imageUri: 'party' },
+  { category: 'Netflix', tasksCount: 7, imageUri: 'netflix_and_chill' },
+];
+
+const tasks = [
+  'Walk the Dog',
+  'Call Dad',
+  'Practice Piano',
+  'Listen to Podcast',
+  'Play Basketball',
+  'Meet with Susan',
+  'Team Meeting',
+  'Grocery Shopping',
+  'Sleep',
+  'Visit John',
+  'Plan Weekend Trip',
+  'Fill the Barrel',
+  'Learn Ga',
+  'Read Bible',
+  'Go to the Barbershop',
+];
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      style={styles.container}
+      data={tasks}
+      renderItem={({ item }) => <TaskItem task={item} />}
+      keyExtractor={(item, index) => index.toString()}
+      ListHeaderComponent={
+        <>
+          <Header username="Wilberforce" tasksCount={16} />
+          <View style={styles.searchContainer}>
+            <View style={styles.inputWrapper}>
+              <Image source={require("./images/search.png")}/>
+              <TextInput style={styles.searchInput} placeholder="Search" />
+            </View>
+            <TouchableOpacity style={styles.filterButton}>
+              <Image style={styles.filterButtonImage} source={require("./images/Filter.png")} />
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.sectionTitle}>Categories</Text>
+          <FlatList
+            horizontal
+            data={categories}
+            renderItem={({ item }) => (
+              <CategoryCard
+                category={item.category}
+                tasksCount={item.tasksCount}
+                imageUri={item.imageUri}
+              />
+            )}
+            keyExtractor={(item, index) => index.toString()}
+            style={styles.categoriesContainer}
+            showsHorizontalScrollIndicator={false}
+          />
+          <Text style={styles.sectionTitle}>Ongoing Task</Text>
+        </>
+      }
+    />
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F7F0E8',
+    paddingTop: 52,
+    paddingBottom: 52,
   },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    marginVertical: 10,
+  },
+  searchInput: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'white',
+    height: 52,
+    border: 'none',
+    marginLeft: -23
+  },
+  sectionTitle: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginVertical: 10,
+    marginHorizontal: 20,
+  },
+  categoriesContainer: {
+    paddingHorizontal: 10,
+  },
+  tasksContainer: {
+    paddingHorizontal: 20
+  },
+  inputWrapper: {
+    backgroundColor: "white",
+    borderColor: '#ccc',
+    borderRadius: 10,
+    marginRight: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    borderRadius: 10,
+    padding:10,
+    gap:20,
+    width:"78%",
+    height: 50,
+  }
 });
+
+export default App;
